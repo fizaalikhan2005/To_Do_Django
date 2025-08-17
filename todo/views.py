0385049f7881b2660c940a63cc81f287905f341b
fizaalikhan2005 <fizaalikhan20051@gmail.com>
2025-08-17 19:30:31 +0500

    Final

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,43 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Task
 # Create your views here.
+
+# Function to add a new task
 def addTask(request):
     task = request.POST['task']
     Task.objects.create(task=task)
     return redirect ('home')
+
+# Function to edit a task
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+# Function to mark a task as undone
+def mark_as_undone(request,pk):
+    task= get_object_or_404(Task, pk=pk)
+    task.is_completed=False
+    task.save()
+    return redirect('home')
+
+# Function to edit a task
+def edit_task(request, pk):
+    get_task = get_object_or_404(Task, pk=pk) 
+    if request.method == 'POST':
+        new_task = request.POST['task']
+        get_task.task = new_task
+        get_task.save()
+        return redirect('home')
+    else:
+        context={
+            'get_task': get_task,
+        }
+        return render(request, 'edit_task.html',context)
+
+# Function to delete a task
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk) 
+    task.delete()
+    return redirect('home')
